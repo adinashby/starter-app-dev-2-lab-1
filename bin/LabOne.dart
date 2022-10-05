903490faeaaa417c824369e2d7748783a3a531b9
Adin Ashby <adin.ashby@gmail.com>
2022-10-05 15:46:48 -0400

    Updated.

---
+++ bin/LabOne.dart
@@ -0,0 +1,25 @@
+/**
+ * Write your test code below in the main (optional).
+ *
+ * @author adinashby
+ */
+
+void main(List<String> arguments) {
+
+}
+
+/**
+ * Please refer to the README file for question(s) description
+ */
+
+void printNumbers() {
+
+}
+
+double findSmallestOutOfThree(double number1, double number2, double number3) {
+  return 0;
+}
+
+int countVowels(String input) {
+  return 0;
+}
